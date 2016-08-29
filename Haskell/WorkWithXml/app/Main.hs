a0e7dcc62ef867ea3b80dfae8069b7e60797d060
Ivan Konovalov <IvanK@firmglobal.com>
2016-08-29 17:22:06 +0300

    Add working with xml example

--- Haskell/WorkWithXml/app/Main.hs
+++ Haskell/WorkWithXml/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
+import System.IO
+
 import Lib
 
-main :: IO ()
-main = someFunc
+
+main :: IO () 
+main =
+   do 
+    handle <- openFile "..\resources\example_survey.xml" ReadMode
+    
+    contents <- hGetContents handle
+    
+    putStr $ process contents
+    putStrLn ""
+    
+    hClose handle

065fc4635ea657bcef35afdd0ff317e331d88805
GiPreduuzie <anavator@yandex.ru>
2017-02-17 09:31:16 +0300

    Templates for properties

---
+++ C#/TemplatesForProperties/TemplatesForProperties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wrapped;
+
+namespace TemplatesForProperties
+{
+    public class MyClass
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Do(MyProperty<string> property)
+        {
+            Console.WriteLine(property.Get());
+            property.Set("new value");
+            Console.WriteLine(property.Get());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(typeof(MyClass).FullName);
+            foreach(var property in typeof(MyClass).GetProperties())
+            {
+                Console.WriteLine(property.PropertyType);
+                //Console.WriteLine(property.ReflectedType);
+
+            }
+
+            Do(new MyClassWrapper(new MyClass() { Property1 = "Hello!" }).Property1);
+        }
+    }
+}
---
+++ C#/TemplatesForProperties/TemplatesForProperties/WrappersGenerator.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+using TemplatesForProperties;
+
+namespace Wrapped{
+
+public class MyProperty<T>
+{
+    T _embedded;
+
+    public MyProperty(Func<T> get, Action<T> set)
+	{
+	    Set = set;
+		Get = get;
+	}
+
+    public Action<T> Set { get; }
+	public Func<T> Get { get; }
+}
+
+public class MyClassWrapper
+{
+	MyClass _embedded;
+
+    public MyClassWrapper(MyClass embedded)
+	{
+	    _embedded = embedded;
+	}
+
+  
+   public MyProperty<System.String> Property1 =>
+       new MyProperty<System.String>(() => _embedded.Property1, v => _embedded.Property1 = v );
+  
+   public MyProperty<System.String> Property2 =>
+       new MyProperty<System.String>(() => _embedded.Property2, v => _embedded.Property2 = v );
+  
+
+}
+}

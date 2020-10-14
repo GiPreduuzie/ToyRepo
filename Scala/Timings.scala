c3412bacf655ef9d890cc4be915f320a746fb47f
Ivan Konovalov <anavator@ya.ru>
2020-10-14 09:12:42 +0300

    add arrya method

--- Scala/Timings.scala
+++ Scala/Timings.scala
@@ -1,24 +1,22 @@
-import java.util
-
 import org.joda.time.DateTime
 
-import scala.collection.GenTraversableOnce
 import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConverters._
 
 
 object Timings {
   def main(args: Array[String]) = {
     val input : List[Option[Int]] = (1 to 10000000).map(Some(_)).toList
 
-    val t1 = timeCycling2 {
+    val t1 = timeCycling2 ("collect") {
       input.collect { case Some(x) => x }
     }
 
-    val t2 = timeCycling2 {
+    val t2 = timeCycling2 ("flat map") {
       input.flatMap(_.toList)
     }
 
-    val t3 = timeCycling2 {
+    val t3 = timeCycling2 ("pattern matching") {
       val result = new ListBuffer[Int]
       input.foreach {
         case Some(v) => result += v
@@ -27,7 +25,7 @@ object Timings {
       result.toList
     }
 
-    val t4 = timeCycling2 {
+    val t4 = timeCycling2 ("with list buffer") {
       val result = new ListBuffer[Int]
       input.foreach { x =>
         if (x.nonEmpty) result += x.get
@@ -35,7 +33,7 @@ object Timings {
       result.toList
     }
 
-    val t5 = timeCycling2 {
+    val t5 = timeCycling2 ("with list recursion") {
       var result = List.empty[Int]
       input.foreach { x =>
         if (x.nonEmpty) result = x.get :: result
@@ -43,40 +41,43 @@ object Timings {
       result
     }
 
-    val t6 = timeCycling2 {
+    val t6 = timeCycling2 ("flatten") {
       input.flatten
     }
 
-
-    val t7 = timeCycling2 {
+    val t7 = timeCycling2 ("with array list") {
       val result = new java.util.ArrayList[Int](input.count(_.isDefined) + 1)
       input.foreach { x =>
         if (x.nonEmpty) result.add(x.get)
       }
-      result
+      result.asScala
     }
 
-    import ToMySeqExtentions._
-
-    val t8 = timeCycling2 {
-      input.collect2(_.isDefined, _.get)
+    val t8 = timeCycling2 ("with array") {
+      val result = new Array[Int](input.count(_.isDefined) + 1)
+      var i = 0
+      input.foreach { x =>
+        if (x.nonEmpty) {
+          result.update(i, x.get)
+          i += 1
+        }
+      }
+      result.toSeq
     }
 
-    println("### t1: " + t1._2)
-    println("### t2: " + t2._2)
-    println("### t3: " + t3._2)
-    println("### t4: " + t4._2)
-    println("### t5: " + t5._2)
-    println("### t6: " + t6._2)
-    println("### t7: " + t7._2)
+    List(t1, t2, t3, t4, t5, t6, t7, t8).foreach { case (name, (_, time)) =>
+      println(s"### t1: $time ms ($name)")
+    }
 
-//    ### t1: 73 538
-//    ### t2: 28 657
-//    ### t3: 94 164
-//    ### t4: 27 395
-//    ### t5: 80 442
-//    ### t6: 78 928
-//    ### t7: 10 668
+//    ### t1: 74266 ms (collect)
+//    ### t1: 28899 ms (flat map)
+//    ### t1: 92293 ms (pattern matching)
+//    ### t1: 29327 ms (with list buffer)
+//    ### t1: 80015 ms (with list recursion)
+//    ### t1: 73811 ms (flatten)
+//    ### t1: 10598 ms (with array list)
+//    ### t1:  6316 ms (with array)
+
   }
 
 
@@ -93,8 +94,8 @@ object Timings {
     time(f)
   }
 
-  def timeCycling2[T](f: => T): (T, Long) = {
-    time((1 to 10).map(_ => f).last)
+  def timeCycling2[T](metric: String)(f: => T): (String, (T, Long)) = {
+    (metric, time((1 to 10).map(_ => f).last))
   }
 
   def time[T](f: => T): (T, Long) = {
